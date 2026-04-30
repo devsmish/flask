642ba49d0eb46fac071b2047e667f3f82068464c
devsmish <smyshynskyi@gmail.com>
2026-04-30 22:22:03 +0200

    add 12-20 tasks in practice03.py

--- ich/10_practice03/practice03.py
+++ ich/10_practice03/practice03.py
@@ -1,5 +1,3 @@
-from itertools import count
-
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, desc, func
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
@@ -110,7 +108,7 @@ with Session() as session:
     print("~" * 16)
 
 """Задача 9: Обновление данных пользователя по ID
-Напишите запрос для обновления данных пользователя, используя его id. Предположим, нужно обновить возраст пользователя 
+Напишите запрос для обновления данных пользователя, используя его id. Предположим, нужно обновить возраст пользователя
 с id равным 5 до 35 лет."""
 with Session() as session:
     user_id = 5
@@ -126,7 +124,7 @@ with Session() as session:
         print("User not found")
 
 """Задача 10: Проверка существования пользователя
-Напишите запрос, который проверяет, существует ли пользователь с заданным name. Проверьте наличие пользователя с name 
+Напишите запрос, который проверяет, существует ли пользователь с заданным name. Проверьте наличие пользователя с name
 равным "Charlie"."""
 with Session() as session:
     username = "Charlie"
@@ -144,3 +142,108 @@ with Session() as session:
 with Session() as session:
     avg_age = session.query(func.avg(User.age)).scalar()
     print("Средний возраст:", avg_age, "лет")
+    print("-" * 16)
+
+"""Задача 12: Максимальный и минимальный возраст
+Создайте запрос, который найдет максимальный и минимальный возраст среди пользователей. Используйте функции func.max() 
+и func.min()."""
+with Session() as session:
+    age_min = session.query(func.min(User.age)).scalar()
+    age_max = session.query(func.max(User.age)).scalar()
+    print(f"Минимальный {age_min} и максимальный {age_max} возраст пользователей")
+    print("-" * 16)
+
+with Session() as session:
+    min_max =  session.query(func.max(User.age).label('max_age'),
+                             func.min(User.age).label('min_age')).one()
+    print(min_max)
+    print("-" * 16)
+
+"""Задача 13: Группировка пользователей по возрасту
+Напишите запрос, который группирует пользователей по возрасту и подсчитывает количество пользователей в каждой 
+возрастной группе."""
+with Session() as session:
+    group_users = session.query(User.age, func.count(User.id)).group_by(User.age).all()
+    print(*[(item[0], item[1]) for item in group_users], sep="\n")
+    print("#" * 16)
+
+with Session() as session:
+    groups_age = session.query(User.age, func.count(User.id)).group_by(User.age).all()
+    for age, count in groups_age:
+        print(f"Age {age}: count {count}")
+        print("-" * 16)
+
+"""Задача 14: Фильтрация групп с использованием having()
+Измените предыдущий запрос так, чтобы он возвращал только те возрастные группы, где количество пользователей больше 1.
+Используйте having() для фильтрации результатов агрегации."""
+with Session() as session:
+    group_users = session.query(User.age, func.count(User.id)).group_by(User.age).having(func.count(User.id) > 1).all()
+    print(*[(item[0], item[1]) for item in group_users], sep="\n")
+    print("#" * 16)
+
+with Session() as session:
+    age_groups = session.query(User.age, func.count(User.id)).group_by(User.age).having(func.count(User.id)>1).all()
+    for age, count in age_groups:
+        print(f"Age: {age}, Count: {count}")
+
+"""Задача 15: Присоединение и подзапросы
+Создайте подзапрос, который вычисляет средний возраст пользователей, а затем напишите основной запрос, который выбирает 
+пользователей старше среднего возраста."""
+with Session() as session:
+    avg_age_subquery = session.query(func.avg(User.age)).scalar_subquery()
+    print("Average age:", avg_age_subquery)
+    older_users = session.query(User).filter(User.age > avg_age_subquery).all()
+    for user in older_users:
+        print(user.name, user.age)
+    print("$" * 16)
+
+with Session() as session:
+    subquery = session.query(func.avg(User.age).label('average_age'), func.max(User.age).label('max_age')).subquery()
+    query = session.query(User).filter(User.age > subquery.c.average_age).all()
+    print(*query)
+
+"""Задача 16: Вывод всех адресов пользователей
+Напишите запрос, который выводит всех пользователей вместе с их адресами."""
+with Session() as session:
+    full_users = session.query(User).outerjoin(Address).all()
+    for user in full_users:
+        for address in user.addresses:
+            print(f"{user.id}. {user.name}: {user.age} from {address.description}")
+    print("%" * 16)
+
+"""Задача 17: Пользователи без адресов
+Напишите запрос, который выводит пользователей, у которых нет адресов."""
+with Session() as session:
+    users_without_adr = session.query(User).outerjoin(Address).filter(Address.id == None).all()
+    print(*users_without_adr)
+    print("^" * 16)
+
+"""Задача 18: Подсчёт пользователей в каждом городе
+Создайте запрос, который подсчитывает количество пользователей в каждом городе."""
+with Session() as session:
+    users_in_city = session.query(Address.description, func.count(User.id)).join(User).group_by(Address.description).all()
+    for city, count in users_in_city:
+        print(f"{city}: {count}")
+
+"""Задача 19: Поиск пользователей по городу
+Напишите запрос для поиска всех пользователей из определённого города, например, из "Berlin"."""
+with Session() as session:
+    berlin_users = session.query(User).join(Address).filter(Address.description == "Berlin").all()
+    for user in berlin_users:
+        for address in user.addresses:
+            print(user.name, address.description)
+        print("-" * 16)
+
+"""Задача 20: Обновление адреса пользователя
+Предположим, что нужно обновить адрес пользователя "Bob" на "Paris". Напишите соответствующий запрос."""
+with Session() as session:
+    user_bob = session.query(User).filter(User.name == "Bob").first()
+    if user_bob:
+        for address in user_bob.addresses:
+            address.description = "Paris"
+        session.commit()
+        print("&" * 16)
+        print(user_bob)
+        print("-" * 16)
+    else:
+        print("User not found")
